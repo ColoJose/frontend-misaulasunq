58a1889fdeb8b431ceabc7ebbfb48689d616be91
Victor <flame.el22@gmail.com>
2020-04-27 23:01:12 -0300

    Avance de prueba de mapa UNQ

--- src/page/App.js
+++ src/page/App.js
@@ -8,7 +8,6 @@ import history from '../utils/history';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
 class App extends React.Component {
   
   render() {
