5aecda36ef2ecfbc13108bb2a914c09418563a1d
jose <msanchez.jose.94@gmail.com>
2020-05-13 11:32:40 -0300

    add model objects

---
+++ src/model/commission.js
@@ -0,0 +1,9 @@
+
+
+export default 
+    Commission = {
+        "name":"",
+        "anio":"",
+        "semestre":"",
+        "schedule": []
+    }
---
+++ src/model/subject.js
@@ -0,0 +1,8 @@
+
+export default 
+    Subject = {
+        "name":"",
+        "subjectCode":"",
+        "commissions":[],
+        "degree":[]
+    }
