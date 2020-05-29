537d9090da35d1bd8eec88ebb0e39eb0f2565cd3
jose <msanchez.jose.94@gmail.com>
2020-05-28 22:27:48 -0300

    ruta privada para admin implementada

--- src/page/App.js
+++ src/page/App.js
@@ -7,6 +7,7 @@ import AdminProfile from '../component/AdminProfile';
 import history from '../utils/history';
 import { Container, Row, Col } from 'react-bootstrap';
 import NewSubjectForm from '../component/newSubject/NewSubjectForm';
+import PrivateRoute from '../component/PrivateRoute';
 
 
 class App extends React.Component {
@@ -24,8 +25,8 @@ class App extends React.Component {
                 <Router history={history}>
                     <Switch>
                         <Route exact path="/" component={Home}  />
-                        <Route exact path="/admin" component={AdminProfile} />
-                        <Route exact path="/admin/newsubjectform" component={NewSubjectForm} />
+                        <PrivateRoute exact path="/admin" render={(props) => <AdminProfile {...props} />} />
+                        <PrivateRoute exact path="/admin/newsubjectform" render={(props) => <NewSubjectForm {...props} />} />
                     </Switch> 
                 </Router>
             </Row>
