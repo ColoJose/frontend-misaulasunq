1995f1b8b90db9b4ebf1f16a8d401790d0e794db
Victor <flame.el22@gmail.com>
2020-05-30 01:10:12 -0300

    Se agrego pathfinding y se adapto el mapa
    Se agrego funcionalidad para buscar y dibujar una ruta al aula destino.
    Se agrego la posibilidad de ver los mapas de los pisos de la universidad con la ruta que le corresponde a cada uno.
    
    Falta agregar la posibilidad de elegir entre la entrada principal y el estacionamiento.
    Falta bloquear los botones de los pisos que no son del aula destino en caso de no encontrar ruta.
    Hay que arreglar los mapas ya que no estan centrados y ajustar las coordenadas.

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,72 +1,129 @@
 import React from 'react';
+import findShortestPath from './../utils/PathFinder';
+import NotFound from './NotFound';
+// bootstrap
+import { ButtonGroup, Button } from 'react-bootstrap';
 // resources
 import UnqMapPB from '../resources/PlantaBajaUnq.png';
 import UnqMapP1 from '../resources/PrimerPisoUnq.png';
 import UnqMapP2 from '../resources/SegundoPisoUnq.png';
 import PinIcon from '../resources/locationIcon.png';
-import Aulas from '../resources/AulasCoord.json';
-import NotFound from './NotFound';
+import Aulas from '../resources/Coords.json';
 
+const MapFloor = Object.freeze({
+    "BAJA":"BAJA", 
+    "PRIMER":"PRIMER", 
+    "SEGUNDO":"SEGUNDO"
+});
 class Map extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.canvas = React.createRef();
-
         this.shouldShowMap = this.shouldShowMap.bind(this)
-        this.draw = this.draw.bind(this);
+        this.draw = this.drawMaps.bind(this);
         this.drawText = this.drawText.bind(this);
         this.drawStroke = this.drawStroke.bind(this);
         this.handleImageLoad = this.handleImageLoad.bind(this);
+        this.mapsReady = this.mapsReady.bind(this);
+        this.drawPin = this.drawPin.bind(this);
+        this.calculateCoord = this.calculateCoord.bind(this);
+        this.drawRoute = this.drawRoute.bind(this);
+        this.drawPath = this.drawPath.bind(this);
+
+        //Canvas Layers reference
+        this.canvasPBMap = React.createRef();
+        this.canvasP1Map = React.createRef();
+        this.canvasP2Map = React.createRef();
+        this.canvasPBPath = React.createRef();
+        this.canvasP1Path = React.createRef();
+        this.canvasP2Path = React.createRef();
+
+        this.pathFromParkingEntrance = [];
+        this.pathFromMainEntrance = [];
+
+        //For canvas and maps images
+        this.defaultWidth = 950;
+        this.defaultHeigth = 558;
 
         this.state = {
-            classRoom: Aulas[props.classroom],
+            mapToShow: MapFloor.BAJA,
             classroomToShow: props.classroom,
-            imageAvailable: this.shouldShowMap(Aulas[props.classroom])
+            imageAvailable: this.shouldShowMap(props.classroom),
+            groundFloorMapLoaded: false,
+            firstFloorMapLoaded: false,
+            secondFloorMapLoaded: false
         };
+
+        //Trato de obtener las rutas del estacionamiento y la entrada principal al aula.
+        if(this.state.imageAvailable){
+            this.pathFromParkingEntrance = findShortestPath(Aulas,"EntradaEstacionamiento",this.state.classroomToShow);
+            this.pathFromMainEntrance = findShortestPath(Aulas,"EntradaPrincipal",this.state.classroomToShow);
+        }
     };
 
-    shouldShowMap(classroom){
+    shouldShowMap(classroomNumber){
+        let classroom = Aulas[classroomNumber];
         return classroom !== null 
             && classroom !== undefined 
-            && ["baja", "primer", "segundo"].includes(classroom.piso);
+            && [MapFloor.BAJA, MapFloor.PRIMER, MapFloor.SEGUNDO].includes(classroom.piso.toUpperCase());
     }
 
-    draw() {
-        let canvas = this.canvas.current;
-
-        if(canvas !== null && canvas !== undefined && this.state.imageAvailable) {
-            let img = new Image(800,500);
-            let pin = new Image(25,25);
-            pin.src = PinIcon;
-
-            let piso = '';
-            switch(this.state.classRoom.piso) {
-                case 'baja':
-                    img.src = UnqMapPB;
-                    piso = 'Planta Baja';
-                    break;
-                case 'primer':
-                    img.src = UnqMapP1;
-                    piso = 'Primer Piso';
-                    break;
-                default:
-                    img.src = UnqMapP2;
-                    piso = 'Segundo Piso';
-            }
-            let pinPosX = this.state.classRoom.x-(pin.width/2);
-            let pinPosY = this.state.classRoom.y-pin.height;
-
-            img.onload = () => this.handleImageLoad(canvas.getContext('2d'),img,pin,piso,pinPosX,pinPosY);
+    drawMaps() {
+        let canvasPB = this.canvasPBMap.current;
+        let canvasP1 = this.canvasP1Map.current;
+        let canvasP2 = this.canvasP2Map.current;
+        
+        //Si canvas es distinto a undefined o null va a ser true
+        if (canvasPB && canvasP1 && canvasP2) {
+            this.createImage(UnqMapPB,canvasPB.getContext('2d'),MapFloor.BAJA);
+            this.createImage(UnqMapP1,canvasP1.getContext('2d'),MapFloor.PRIMER);
+            this.createImage(UnqMapP2,canvasP2.getContext('2d'),MapFloor.SEGUNDO);
         };
     };
 
-    handleImageLoad(canvasContext,map,pin,floor,pinPosX,pinPosY){
-        canvasContext.width = 800;
-        canvasContext.height = 500;
-        canvasContext.drawImage(map, 0, 0,map.width,map.height);
-        canvasContext.drawImage(pin, pinPosX, pinPosY,pin.width,pin.height);
+    createImage(image,canvasContext,floor){
+        let floorMap = new Image(this.defaultWidth,this.defaultHeigth);
+        floorMap.src = image;
+        floorMap.onload = () => this.handleImageLoad(canvasContext,floorMap,floor);
+    };
 
+    handleImageLoad(canvasContext,map,floor){
+        canvasContext.globalCompositeOperation = "destination-over";
+        canvasContext.width = this.defaultWidth;
+        canvasContext.height = this.defaultHeigth;
+        canvasContext.drawImage(map, 0, 0,map.width,map.height);
+        
+        switch (floor) {
+            case MapFloor.BAJA:
+                this.setState({ groundFloorMapLoaded: true  }, this.mapsReady);
+                break;
+            case MapFloor.PRIMER:
+                this.setState({ firstFloorMapLoaded: true  }, this.mapsReady);
+                break
+            default:
+                this.setState({ secondFloorMapLoaded: true  }, this.mapsReady);
+                break
+        }
+    };
+
+    mapsReady(){
+        if(this.state.groundFloorMapLoaded && this.state.firstFloorMapLoaded && this.state.secondFloorMapLoaded){
+            this.props.mapReady();
+        }
+    };
+
+    drawPin(canvasContext,percentCoordX,percentCoordY){
+        let pin = new Image(25,25);
+        pin.src = PinIcon;
+        let coordX = this.calculateCoord(this.defaultWidth, percentCoordX);
+        let coordY = this.calculateCoord(this.defaultHeigth, percentCoordY);
+        let pinPosX = coordX-(pin.width/2);
+        let pinPosY = coordY-pin.height;
+        pin.onload = () => {
+            canvasContext.globalCompositeOperation = "source-over";
+            canvasContext.drawImage(pin, pinPosX, pinPosY,pin.width,pin.height);
+        };
+        
         // Nombre aula
         this.drawText(
             canvasContext, 
@@ -75,7 +132,7 @@ class Map extends React.Component {
             0.60, 
             "#bd2130", 
             this.state.classroomToShow, 
-            this.state.classRoom.x, 
+            coordX, 
             pinPosY-5
         );
         this.drawStroke(
@@ -83,25 +140,17 @@ class Map extends React.Component {
             0.60, 
             'black', 
             this.state.classroomToShow, 
-            this.state.classRoom.x, 
+            coordX, 
             pinPosY-5
         );
-        // Nombre Piso
-        this.drawText(
-            canvasContext, 
-            '32px arial', 
-            "center", 
-            0.60, 
-            'black', 
-            floor, 
-            canvasContext.width/2, 
-            32
-        );
-        
-        this.props.mapReady();
-    }
+    };
+
+    calculateCoord(size, porcentualCoord){
+        return size * (porcentualCoord / 100);
+    };
 
     drawText(canvasContext, font, align, lineWidth, style, floor, posX, posY){
+        canvasContext.globalCompositeOperation = "source-over";
         canvasContext.font = font;
         canvasContext.textAlign = align;
         canvasContext.lineWidth = lineWidth;
@@ -110,28 +159,123 @@ class Map extends React.Component {
     };
 
     drawStroke(canvasContext, lineWidth, style, classroomNumber, posX, posY){
+        canvasContext.globalCompositeOperation = "source-over";
         canvasContext.lineWidth = lineWidth;
         canvasContext.strokeStyle = style;
         canvasContext.strokeText(classroomNumber, posX, posY);
     };
 
+    drawRoute(path){
+        let canvasContextPB = this.canvasPBMap.current.getContext('2d');
+        let canvasContextP1 = this.canvasP1Map.current.getContext('2d');
+        let canvasContextP2 = this.canvasP2Map.current.getContext('2d');
+
+        let actualNode;
+        let nextNode;
+        for(var i=0; i < path.length-2; i++){
+            actualNode = Aulas[path[i]];
+            nextNode = Aulas[path[i+1]];
+
+            switch (actualNode.piso.toUpperCase()) {
+                case 'BAJA':
+                    this.drawPath(canvasContextPB, actualNode, nextNode);
+                    break;
+                case 'PRIMER':
+                    this.drawPath(canvasContextP1, actualNode, nextNode);
+                    break
+                default:
+                    this.drawPath(canvasContextP2, actualNode, nextNode);
+                    break;
+            }
+        }
+    }
+
+    drawPath(canvasContext, actualNode, nextNode){
+        canvasContext.beginPath();
+        canvasContext.lineWidth = "3";
+        canvasContext.strokeStyle = "#bd2130";
+        canvasContext.moveTo(
+            this.calculateCoord(this.defaultWidth,actualNode.x),
+            this.calculateCoord(this.defaultHeigth,actualNode.y));
+        canvasContext.lineTo(
+            this.calculateCoord(this.defaultWidth,nextNode.x),
+            this.calculateCoord(this.defaultHeigth,nextNode.y));
+        canvasContext.stroke();
+    }
+
     componentDidMount() {
-        this.draw();
+        if(this.state.imageAvailable){
+            this.drawMaps();
+            let classroom = Aulas[this.state.classroomToShow];
+            let route = [];
+
+            //si hay ruta desde la entrada principal la cargo, sino desde el estacionamiento
+            if(Array.isArray(this.pathFromMainEntrance) && this.pathFromMainEntrance.length){
+                route = this.pathFromMainEntrance;
+            }else{
+                route = this.pathFromParkingEntrance;
+            }
+
+            if(Array.isArray(route) && route.length){
+                this.drawRoute(route);
+            }
+
+            switch (classroom.piso.toUpperCase()) {
+                case 'BAJA':
+                    this.drawPin(this.canvasPBMap.current.getContext('2d'),classroom.x,classroom.y);
+                    this.setState({ mapToShow: MapFloor.BAJA });
+                    break;
+                case 'PRIMER':
+                    this.drawPin(this.canvasP1Map.current.getContext('2d'),classroom.x,classroom.y);
+                    this.setState({ mapToShow: MapFloor.PRIMER });
+                    break
+                default:
+                    this.drawPin(this.canvasP2Map.current.getContext('2d'),classroom.x,classroom.y);
+                    this.setState({ mapToShow: MapFloor.SEGUNDO });
+                    break;
+            }
+        } else {
+            this.props.mapReady();
+        }
     };
 
+    /* TODO: Se podria usar un canvas para el mapa y otro para la ruta, asi cuando se tiene que elegir 
+    entre la entrada principal o el estacionamiento solo se cambiaria el canvas de la ruta */
     render(){
         if(this.state.imageAvailable){
             return (
                 <>
-                    <canvas id="map"
-                            ref={this.canvas}
-                            width="800"
-                            height="500">
+                    <ButtonGroup size="sm">
+                        <Button onClick={ ()=> {this.setState({mapToShow:MapFloor.BAJA})}}>Planta Baja</Button>
+                        <Button onClick={ ()=> {this.setState({mapToShow:MapFloor.PRIMER})}}>Primer Piso</Button>
+                        <Button onClick={ ()=> {this.setState({mapToShow:MapFloor.SEGUNDO})}}>Segundo Piso</Button>
+                    </ButtonGroup>
+                    <br />
+                    <canvas id="mapPB"
+                            ref={this.canvasPBMap}
+                            width={this.defaultWidth}
+                            height={this.defaultHeigth}
+                            hidden={ this.state.mapToShow !== MapFloor.BAJA}>
+                        El navegador no soporta el método de visualización.
+                    </canvas>
+                    <canvas id="mapP1"
+                            ref={this.canvasP1Map}
+                            width={this.defaultWidth}
+                            height={this.defaultHeigth}
+                            hidden={ this.state.mapToShow !== MapFloor.PRIMER}>
+                        El navegador no soporta el método de visualización.
+                    </canvas>
+                    <canvas id="mapP2"
+                            ref={this.canvasP2Map}
+                            width={this.defaultWidth}
+                            height={this.defaultHeigth}
+                            hidden={ this.state.mapToShow !== MapFloor.SEGUNDO}>
+                        El navegador no soporta el método de visualización.
                     </canvas>
                 </>
             );
         } else {
-            return <NotFound label={"Ubicación No Disponible"}/>
+            return <NotFound label={"Ubicación No Disponible."}/>
         }
     };
 }
--- src/component/MapModal.jsx
+++ src/component/MapModal.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 // Bootstrap
 import { Button, Modal } from "react-bootstrap";
 // Resources
@@ -7,14 +7,22 @@ import Map from './Map';
 import { BsGeoAlt } from 'react-icons/bs';
 // Semantic UI
 import { Dimmer, Loader } from 'semantic-ui-react';
+// css
+import './MapModal.css';
 
 function MapModal({classRoomNumber}){
-    const [show, setShow] = useState(false);
-    const [loading, setLoading] = useState(true);
 
-    const handleReady = () => setLoading(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [state, setState] = useReducer((state, newState) => ({...state, ...newState}),{show: false, loading: true});
+  const handleReady = () => setState({loading: false});
+  const handleClose = () => setState({show: false, loading: true});
+  const handleShow = () => setState({show: true});
+
+    // const [show, setShow] = useState(false);
+    // const [loading, setLoading] = useState(true);
+
+    // const handleReady = () => setLoading(false);
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
 
     //TODO: Evaluar si se separa en componetes mas pequeños
     return (
@@ -24,7 +32,7 @@ function MapModal({classRoomNumber}){
               <BsGeoAlt size='1em'/>
           </Button>
 
-          <Modal show={show} 
+          <Modal show={state.show} 
                  onHide={handleClose}
                  size="xl"
                  aria-labelledby="contained-modal-title-vcenter"
@@ -35,7 +43,7 @@ function MapModal({classRoomNumber}){
               </Modal.Title>
             </Modal.Header>
             <Modal.Body className="text-center">
-              <Dimmer active={loading} inverted>
+              <Dimmer active={state.loading} inverted>
                   <Loader indeterminate>Cargando Mapa</Loader>
               </Dimmer>
               <Map classroom={classRoomNumber}
